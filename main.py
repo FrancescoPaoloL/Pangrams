db27ed9a49f47682e944ef66e5efa0616b77e036
fpl <francescopaololezza@gmail.com>
2023-06-18 21:02:53 +0200

    Added main part

---
+++ main.py
@@ -0,0 +1,9 @@
+from pangram import generate_pangram
+
+def main():
+    # Generate and print a pangram with meaningful sentences
+    pangram = generate_pangram()
+    print(pangram)
+
+if __name__ == "__main__":
+    main()
